e356229ccc6dbe83eea492ae1960806fa9f7e1e4
mulirous <muriloalimacosta@hotmail.com>
2025-06-14 11:15:27 -0300

    tarefa15: v3

--- tarefa15/sobreposicao.c
+++ tarefa15/sobreposicao.c
@@ -16,19 +16,19 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int local_n = N / size + 2; // +2 para células fantasmas
-    double *atual = malloc(local_n * sizeof(double));
-    double *anterior = malloc(local_n * sizeof(double));
+    double *current = malloc(local_n * sizeof(double));
+    double *previous = malloc(local_n * sizeof(double));
 
     // Inicializa
-    memset(atual, 0, local_n * sizeof(double));
-    memset(anterior, 0, local_n * sizeof(double));
+    memset(current, 0, local_n * sizeof(double));
+    memset(previous, 0, local_n * sizeof(double));
 
     if (rank == 0)
         for (int i = 1; i < local_n - 1; i++)
-            anterior[i] = 100.0; // Calor no início da barra
+            previous[i] = 100.0; // Calor no início da barra
 
     MPI_Barrier(MPI_COMM_WORLD);
-    double start = MPI_WTime();
+    double start = MPI_Wtime();
 
     for (int t = 0; t < T; t++)
     {
@@ -38,31 +38,31 @@ int main(int argc, char **argv)
         int flag;
 
         if (rank > 0)
-            MPI_Isend(&anterior[1], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &reqs[0]);
+            MPI_Isend(&previous[1], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &reqs[0]);
         if (rank < size - 1)
-            MPI_Irecv(&anterior[local_n - 1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &reqs[1]);
+            MPI_Irecv(&previous[local_n - 1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &reqs[1]);
 
         if (rank < size - 1)
-            MPI_Isend(&anterior[local_n - 2], 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &reqs[2]);
+            MPI_Isend(&previous[local_n - 2], 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &reqs[2]);
         if (rank > 0)
-            MPI_Irecv(&anterior[0], 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &reqs[3]);
+            MPI_Irecv(&previous[0], 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &reqs[3]);
 
         // Cálculo dos pontos internos (sem depender de bordas)
         for (int i = 2; i < local_n - 2; i++)
         {
-            atual[i] = anterior[i] + ALPHA * (anterior[i - 1] - 2 * anterior[i] + anterior[i + 1]);
+            current[i] = previous[i] + ALPHA * (previous[i - 1] - 2 * previous[i] + previous[i + 1]);
         }
 
         // Verifica se as comunicações de borda terminaram
         if (rank > 0)
         {
             MPI_Test(&reqs[3], &flag, MPI_STATUS_IGNORE); // esquerda
-            atual[1] = anterior[1] + ALPHA * (anterior[0] - 2 * anterior[1] + anterior[2]);
+            current[1] = previous[1] + ALPHA * (previous[0] - 2 * previous[1] + previous[2]);
         }
         if (rank < size - 1)
         {
             MPI_Test(&reqs[1], &flag, MPI_STATUS_IGNORE); // direita
-            atual[local_n - 2] = anterior[local_n - 2] + ALPHA * (anterior[local_n - 3] - 2 * anterior[local_n - 2] + anterior[local_n - 1]);
+            current[local_n - 2] = previous[local_n - 2] + ALPHA * (previous[local_n - 3] - 2 * previous[local_n - 2] + previous[local_n - 1]);
         }
 
         if (t % 100 == 0)
@@ -70,9 +70,9 @@ int main(int argc, char **argv)
             printf("Rank %d - Iteração %d - Temp. centro local: %.2f\n", rank, t, previous[local_n / 2]);
         }
 
-        double *tmp = anterior;
-        anterior = atual;
-        atual = tmp;
+        double *tmp = previous;
+        previous = current;
+        current = tmp;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
