242ac4a01dac58162234fa67c7571f6d216e0f4f
mulirous <muriloalimacosta@hotmail.com>
2025-06-14 15:54:11 -0300

    tarefa16: v8

--- tarefa16/vetorizacao.c
+++ tarefa16/vetorizacao.c
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Testes para diferentes valores de N (assumindo M = N para simplificar)
-    int Ns[] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+    int Ns[] = {2, 4, 8, 16, 32, 64, 128, 256, 512};
     for (int i = 0; i < sizeof(Ns) / sizeof(Ns[0]); i++)
     {
         int N = Ns[i];
