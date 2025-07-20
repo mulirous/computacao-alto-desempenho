b71707e27eb304f5579cb239e1fb1f50ba6b20b3
mulirous <muriloalimacosta@hotmail.com>
2025-07-20 01:45:43 -0300

    tarefa 23: v2

--- readme.md
+++ readme.md
@@ -6,6 +6,20 @@
 nvc -mp=gpu -o heat heat.c
 ```
 
+## Compilação CUDA
+
+```shell
+nvcc -o meu_programa meu_programa.cu
+```
+
+## Uso do nsys
+
+### Seu uso em qualquer executável
+
+```shell
+nsys ./executavel
+```
+
 ### Visualização de arquivo nsys
 
 ```shell
--- tarefa23/navier_cuda_shared.cu
+++ tarefa23/navier_cuda_shared.cu
@@ -50,53 +50,53 @@ __global__ void navier_cuda(double *u_new, double *u, int N) {
 }
 
 
-int main() {
-    int sizes[] = {128, 512, 1024, 2048, 4096}; // Pode expandir!
-    int nsizes = sizeof(sizes)/sizeof(int);
+int main(int argc, char **argv) {
 
-    for (int s = 0; s < nsizes; ++s) {
-        int N = sizes[s];
-        size_t size = N*N*sizeof(double);
-
-        double *h_u = (double*)calloc(N*N, sizeof(double));
-        double *h_u_new = (double*)calloc(N*N, sizeof(double));
-        h_u[(N/2)*N + (N/2)] = 1000.0;
-
-        double *d_u, *d_u_new;
-        cudaMalloc(&d_u, size);
-        cudaMalloc(&d_u_new, size);
-        cudaMemcpy(d_u, h_u, size, cudaMemcpyHostToDevice);
-
-        dim3 block(16, 16);
-        dim3 grid((N+15)/16, (N+15)/16);
-
-        cudaEvent_t start, stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-
-        cudaEventRecord(start);
-        for (int t=0; t<NSTEPS; t++) {
-            navier_cuda<<<grid, block>>>(d_u_new, d_u, N);
-            double *tmp = d_u; d_u = d_u_new; d_u_new = tmp;
-        }
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-
-        float ms = 0;
-        cudaEventElapsedTime(&ms, start, stop);
-
-        // Log SLURM-friendly:
-        printf("Tamanho: %d x %d | Tempo GPU: %.4f ms\n", N, N, ms);
-
-        cudaMemcpy(h_u, d_u, size, cudaMemcpyDeviceToHost);
-
-        // (Opcional) imprimir soma ou valor central para comparação
-        printf("u[%d][%d] = %f\n", N/2, N/2, h_u[(N/2)*N + (N/2)]);
-
-        free(h_u); free(h_u_new);
-        cudaFree(d_u); cudaFree(d_u_new);
-        cudaEventDestroy(start);
-        cudaEventDestroy(stop);
+    if (argc < 2) {
+        printf("Uso: %s <tamanho_malha>\n", argv[0]);
+        return 1;
     }
+    int N = atoi(argv[1]);
+    size_t size = N*N*sizeof(double);
+
+    double *h_u = (double*)calloc(N*N, sizeof(double));
+    double *h_u_new = (double*)calloc(N*N, sizeof(double));
+    h_u[(N/2)*N + (N/2)] = 1000.0;
+
+    double *d_u, *d_u_new;
+    cudaMalloc(&d_u, size);
+    cudaMalloc(&d_u_new, size);
+    cudaMemcpy(d_u, h_u, size, cudaMemcpyHostToDevice);
+
+    dim3 block(16, 16);
+    dim3 grid((N+15)/16, (N+15)/16);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    for (int t=0; t<NSTEPS; t++) {
+        navier_cuda<<<grid, block>>>(d_u_new, d_u, N);
+        double *tmp = d_u; d_u = d_u_new; d_u_new = tmp;
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float ms = 0;
+    cudaEventElapsedTime(&ms, start, stop);
+
+    // Log SLURM-friendly:
+    printf("Tamanho: %d x %d | Tempo GPU: %.4f ms\n", N, N, ms);
+
+    cudaMemcpy(h_u, d_u, size, cudaMemcpyDeviceToHost);
+
+    // (Opcional) imprimir soma ou valor central para comparação
+    printf("u[%d][%d] = %f\n", N/2, N/2, h_u[(N/2)*N + (N/2)]);
+
+    free(h_u); free(h_u_new);
+    cudaFree(d_u); cudaFree(d_u_new);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
     return 0;
 }
